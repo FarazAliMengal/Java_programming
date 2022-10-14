5f8bdd46b560f2c8c11bb303b9df5c8139021bfd
Faraz Ali <115438020+FarazAliMengal@users.noreply.github.com>
2022-10-14 06:12:10 -0700

    Add files via upload

---
+++ Bool.java
@@ -0,0 +1,21 @@
+class Bool
+{
+          public static void main(String args[])
+          {
+                 int a=100;
+                 int b=200;
+                 boolean c;
+                 c=(a>b);
+                 System.out.println(c);
+                 c=(b>a);
+                 System.out.println(c);
+                 c=(a==b);
+                 System.out.println(c);
+                 c=(a>=b);
+                 System.out.println(c);
+                 c=(b>=a);
+                 System.out.println(c);
+                 c=(a!=b);
+                 System.out.println(c);
+         }
+}
