3f37507e5ab0240077ed1fcffb89dbd6540a8ec8
Faraz Ali <115438020+FarazAliMengal@users.noreply.github.com>
2022-10-22 17:45:12 +0500

    Add files via upload

---
+++ Task8.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Task8
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+
+		System.out.print("Enter 1st Number:");
+		int x=sc.nextInt();
+
+		System.out.print("Enter 2nd Number:");
+		int y=sc.nextInt();
+
+		System.out.println("Addition is:"+(x+y));
+		System.out.println("Subtraction is:"+(x-y));
+		System.out.println("Multiplication is:"+(x*y));
+		System.out.println("Division is:"+(x/y));
+		System.out.println("Module is:"+(x%y));
+	}
+
+}     
