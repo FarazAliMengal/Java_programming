7b954faf70fb4159c6a2399dcc0ba8579087825e
Faraz Ali <115438020+FarazAliMengal@users.noreply.github.com>
2022-10-22 18:43:32 +0500

    Add files via upload

---
+++ task7f.java
@@ -0,0 +1,25 @@
+class task7f
+{
+	public static void main(String args[])
+	{
+		String a="Pakistan";
+		String b="Zindabad";
+ 		System.out.println("NEW LINE ESCAPE CHARACTER");
+		System.out.println("Pakistan\n"+"Zindabad");
+		System.out.println("BACK ESCAPE CHARACTER");
+		System.out.println("Pakistan\b"+"Zindabad");
+		System.out.println("TAB ESCAPE CHARACTER");
+		System.out.println("Pakistan\t"+"Zindabad");
+		System.out.println("SINGLE QOUTE ESCAPE CHARACTER");
+		System.out.println("Pakistan\'"+"Zindabad");
+		System.out.println("DOUBLE QOUTE ESCAPE CHARACTER");
+		System.out.println("Pakistan\""+"Zindabad");
+		System.out.println("CARAIGE ESCAPE CHARACTER");
+		System.out.println("Pakistan\r"+"Zindabad");
+		System.out.println("BACK SLASH ESCAPE");
+		System.out.println("Pakistan\\"+"Zindabad");
+
+	}
+
+}
+		
