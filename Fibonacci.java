fb4487ebe48daf3139dc16fa58164437cb408c3d
Faraz Ali <115438020+FarazAliMengal@users.noreply.github.com>
2022-10-29 11:55:37 +0500

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,26 @@
+class Fibonacci
+{
+	public static void main(String args[])
+	{
+		while(true){
+		int a=0,b=1,c;
+		int i;
+
+		for(i=1;i<40;i++)
+		{
+		System.out.println(" "+a);
+		c=a+b;	
+		a=b;
+		b=c;
+		
+		}
+			   }
+
+	}
+
+}
+
+	
+		
+
+	
